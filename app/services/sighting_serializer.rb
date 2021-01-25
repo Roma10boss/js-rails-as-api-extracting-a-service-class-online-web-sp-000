7f4c68fab7b9c28399f43b9c8a549dd3e7e9822b
Romario Gustave <romagustave10@gmail.com>
2021-01-25 01:58:11 +0000

    Automatically backed up by Learn

--- app/services/sighting_serializer.rb
+++ app/services/sighting_serializer.rb
@@ -1,28 +1,11 @@
 class SightingSerializer
   def index
-    def initialize(sighting_object)
-      @sighting = sighting_object
-    end
-  
-  def to_serialized_json
-    @sighting.to_json(:include => {
-      :bird => {:only => [:name, :species]},
-      :location => {:only => [:latitude, :longitude]}
-    }, :except => [:updated_at])
-  end
-  
     sightings = Sighting.all
-    render json: sightings.to_json(:include => {
-      :bird => {:only => [:name, :species]},
-      :location => {:only => [:latitude, :longitude]}
-    }, :except => [:updated_at])
+    render json: SightingSerializer.new(sightings).to_serialized_json
   end
  
   def show
     sighting = Sighting.find_by(id: params[:id])
-    render json: sighting.to_json(:include => {
-      :bird => {:only => [:name, :species]},
-      :location => {:only => [:latitude, :longitude]}
-    }, :except => [:updated_at])
+    render json: SightingSerializer.new(sighting).to_serialized_json
   end
 end
